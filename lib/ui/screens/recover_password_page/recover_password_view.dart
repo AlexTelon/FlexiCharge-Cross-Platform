356a2e79a613bd6228c3f3da85f064858591b9ba
AbdoMallah <you@example.com>
2021-09-26 15:59:49 +0200

    Created Recover_Password UI

---
+++ lib/ui/screens/recover_password_page/recover_password_view.dart
@@ -0,0 +1,80 @@
+import 'package:flexicharge/ui/screens/home_page/home_viewmodel.dart';
+import 'package:flexicharge/ui/screens/recover_password_page/recover_password_viewmodel.dart';
+import 'package:flexicharge/ui/widgets/text_input.dart';
+import 'package:flexicharge/ui/widgets/top_bar.dart';
+import 'package:flexicharge/ui/widgets/wide_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:stacked/stacked.dart';
+
+class RecoverPasswordView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<RecoverPasswordViewModel>.reactive(
+      viewModelBuilder: () => RecoverPasswordViewModel(),
+      builder: (context, model, child) => Scaffold(
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(
+                flex: 2,
+                child: Topbar(
+                  text: "Recover Password",
+                  onTap: () => print("Back to previos page..."),
+                ),
+              ),
+              // UPPP
+              // SizedBox(height: 30),
+              Flexible(
+                flex: 4,
+                child: Container(
+                  child: Column(
+                    // crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Flexible(
+                        flex: 5,
+                        child: Column(
+                          children: [
+                            TextInputWidget(
+                              labelText: 'Email',
+                              hint: 'Enter Your Email',
+                              onChanged: (value) => print(value),
+                            ),
+                            SizedBox(height: 30),
+                            Text(
+                                '''Please provide the email address you used to register.\nWe will send you an email.\nWith a link to reset your password''',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontFamily: 'Lato',
+                                  color: Color(0xff212121),
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.w400,
+                                  fontStyle: FontStyle.normal,
+                                )),
+                            // Text('We will send you an email.'),
+                            // Text('with a link to reset your password')
+                          ],
+                        ),
+                      ),
+                      Flexible(
+                        flex: 1,
+                        child: WideButton(
+                          text: 'Send',
+                          showWideButton: true,
+                          onTap: () => ('Send'),
+                          color: Color(0xff78bd76),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
