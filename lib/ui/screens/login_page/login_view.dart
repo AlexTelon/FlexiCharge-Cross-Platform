f076a921af1228eb43c208f8e84d3aef971da88c
AbdoMallah <you@example.com>
2021-09-26 14:59:22 +0200

    Flexiable instead of SizedBox
    Used Flexible instead of using SizedBox
    between the input and login button

--- lib/ui/screens/login_page/login_view.dart
+++ lib/ui/screens/login_page/login_view.dart
@@ -18,61 +18,73 @@ class LoginView extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Topbar(
-                text: "Log In",
-                onTap: () => print("Back to previos page..."),
+              Flexible(
+                flex: 2,
+                child: Topbar(
+                  text: "Log In",
+                  onTap: () => print("Back to previos page..."),
+                ),
               ),
               // UPPP
-              SizedBox(height: 30),
-              Container(
-                child: Column(
-                  // crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    ///Contains the TextInputs
-                    Column(
-                      children: [
-                        TextInputWidget(
-                          labelText: 'Email',
-                          hint: 'Enter Your Email',
-                          onChanged: (value) => print(value),
-                        ),
-                        SizedBox(height: 30),
-                        TextInputWidget(
-                          labelText: 'Password',
-                          hint: 'Enter Your Password',
-                          onChanged: (value) => print(value),
-                          isPassword: true,
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 200),
-
-                    ///Contains the WideButton and The InkWell
-                    Column(
-                      children: [
-                        WideButton(
-                          text: 'Log in',
-                          showWideButton: true,
-                          onTap: () => ('Log in'),
-                          color: Color(0xff78bd76),
-                        ),
-                        SizedBox(height: 30),
-                        InkWell(
-                          onTap: () => print('Forget Password'),
-                          child: Text(
-                            'I forget my password',
-                            style: TextStyle(
-                              fontFamily: 'Lato',
-                              color: Color(0xff78bd76),
-                              fontSize: 13,
-                              fontWeight: FontWeight.w700,
-                              fontStyle: FontStyle.normal,
+              // SizedBox(height: 30),
+              Flexible(
+                flex: 4,
+                child: Container(
+                  child: Column(
+                    // crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ///Contains the TextInputs
+                      Flexible(
+                        flex: 3,
+                        child: Column(
+                          children: [
+                            TextInputWidget(
+                              labelText: 'Email',
+                              hint: 'Enter Your Email',
+                              onChanged: (value) => print(value),
                             ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ],
+                            SizedBox(height: 30),
+                            TextInputWidget(
+                              labelText: 'Password',
+                              hint: 'Enter Your Password',
+                              onChanged: (value) => print(value),
+                              isPassword: true,
+                            ),
+                          ],
+                        ),
+                      ),
+                      // SizedBox(height: 200),
+
+                      ///Contains the WideButton and The InkWell
+                      Flexible(
+                        flex: 1,
+                        child: Column(
+                          children: [
+                            WideButton(
+                              text: 'Log in',
+                              showWideButton: true,
+                              onTap: () => ('Log in'),
+                              color: Color(0xff78bd76),
+                            ),
+                            SizedBox(height: 30),
+                            InkWell(
+                              onTap: () => print('Forget Password'),
+                              child: Text(
+                                'I forget my password',
+                                style: TextStyle(
+                                  fontFamily: 'Lato',
+                                  color: Color(0xff78bd76),
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.w700,
+                                  fontStyle: FontStyle.normal,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
