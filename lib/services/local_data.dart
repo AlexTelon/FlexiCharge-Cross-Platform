780731159e527fa8038de44a638da955b1f0a208
Didrik Nordström <diddenordstrom@gmail.com>
2021-09-29 13:13:43 +0200

    Readded the needed local variable for chargers

--- lib/services/local_data.dart
+++ lib/services/local_data.dart
@@ -1,6 +1,8 @@
+import 'package:flexicharge/models/charger.dart';
 import 'package:flexicharge/models/charger_point.dart';
 
 class LocalData {
+  List<Charger> chargers = [];
   String qrCode = '';
   List<ChargerPoint> chargerPoints = [];
 }
