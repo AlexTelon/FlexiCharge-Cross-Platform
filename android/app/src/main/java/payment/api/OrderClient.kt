f3d96f8d9e3ffd6e58855b8e6d8ead5cdb2c1e4d
abah19vo <70108548+abah19vo@users.noreply.github.com>
2021-10-13 23:16:37 +0200

    new keys

--- android/app/src/main/java/payment/api/OrderClient.kt
+++ android/app/src/main/java/payment/api/OrderClient.kt
@@ -4,8 +4,8 @@ package payment.api
 
 object OrderClient {
 
-    private const val user = "PK44717_4c1b90e45244" // please enter user here
-    private const val password = "RfrpsR51RQUghWf1" // please enter password here
+    private const val user = "PK44717_c4725bec1088" // please enter user here
+    private const val password = "QnkVkiprxTaA3aDN" // please enter password here
    
     private const val baseUrl = "https://api.playground.klarna.com/" // please update the url according to your location and desired environment (playground, production)
     fun hasSetCredentials() = user.isNotBlank() && password.isNotBlank()
