1f56ba4c5cf540e4cdfb81fcbd3622ba645e41c7
Gustav Persson <mailtogustav7@gmail.com>
2022-09-21 12:45:17 +0200

    Create example file for unit tests

---
+++ test/unit_test.dart
@@ -0,0 +1,14 @@
+// This is a basic Flutter unit test.
+//
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test("Example unit test description", () async {
+    //ARRANGE
+
+    //ACT
+
+    //ASSERT
+  });
+}
