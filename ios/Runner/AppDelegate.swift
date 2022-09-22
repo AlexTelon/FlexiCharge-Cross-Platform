347fc74c9c8783620cca225c69a3410e595dd3e1
Gustav Persson <mailtogustav7@gmail.com>
2022-09-22 22:50:41 +0200

    Add code for starting Klarna payment view on iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -12,6 +12,21 @@ import GoogleMaps
   ) -> Bool {
     GMSServices.provideAPIKey("AIzaSyAsWiiZLhjHhTj830uzF-LsmXZFfrbl8g4")
     GeneratedPluginRegistrant.register(with: self)
+      
+      let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+      
+      let METHOD_CHANNEL_NAME = "com.startActivity/klarnaChannel"
+      
+      let klarnaChannel = FlutterMethodChannel(name: METHOD_CHANNEL_NAME, binaryMessenger: controller.binaryMessenger)
+      
+      klarnaChannel.setMethodCallHandler({
+          (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+          
+          if (call.method == "StartKlarnaActivity"){
+              //TODO show Klarna payment view
+          }
+      })
+      
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
