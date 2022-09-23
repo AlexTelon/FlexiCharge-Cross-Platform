6d60c23dc2b071d52a026004b646891f2ad221fc
Gustav Persson <mailtogustav7@gmail.com>
2022-09-23 12:31:49 +0200

    Copy and paste Klarna View and ViewModel from iOS team's repository

--- ios/Runner/KlarnaPaymentView.swift
+++ ios/Runner/KlarnaPaymentView.swift
@@ -1,20 +1,63 @@
 //
 //  KlarnaPaymentView.swift
-//  Runner
+//  FlexiCharge
 //
-//  Created by Gustav Persson on 2022-09-23.
+//  Created by Daniel Göthe on 2021-09-27.
 //
-
+// Modified by Gustav Persson on 2022-09-23.
+//
+import UIKit
 import SwiftUI
+import KlarnaMobileSDK
 
-struct KlarnaPaymentView: View {
+
+public protocol ViewControllerDelegate: AnyObject {
+    func displayPaymentView()
+}
+
+
+struct KlarnaView: View {
+    @Binding var isPresented: Bool
+    @Binding var klarnaStatus: String
+    @Binding var chargerIdInput: String
+    @Binding var transactionID: Int
+    @ObservedObject var sdkIntegration: KlarnaSDKIntegration = KlarnaSDKIntegration()
+
+    init(isPresented: Binding<Bool>, klarnaStatus: Binding<String>, chargerIdInput: Binding<String>, transactionID: Binding<Int>) {
+        self._isPresented = isPresented
+        self._klarnaStatus = klarnaStatus
+        self._chargerIdInput = chargerIdInput
+        self._transactionID = transactionID
+        sdkIntegration.getKlarnaSession(chargerIdInput: chargerIdInput)
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            Image("klarna-logo-pink")
+            Spacer()
+            ProgressView().progressViewStyle(CircularProgressViewStyle())
+            Spacer()
+            Spacer()
+        }.onChange(of: sdkIntegration.isKlarnaPaymentDone, perform: { _ in
+            klarnaStatus = sdkIntegration.klarnaStatus
+            transactionID = sdkIntegration.thisTransactionID ?? 0
+            isPresented = false
+        })
     }
 }
 
-struct KlarnaPaymentView_Previews: PreviewProvider {
-    static var previews: some View {
-        KlarnaPaymentView()
+
+struct InitializeKlarna: UIViewControllerRepresentable, View {
+    @Binding var isPresented: Bool
+    @Binding var klarnaStatus: String
+    @Binding var chargerIdInput: String
+    @Binding var transactionID: Int
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<InitializeKlarna>) -> UIHostingController<KlarnaView> {
+        return UIHostingController(rootView: KlarnaView(isPresented: $isPresented, klarnaStatus: $klarnaStatus, chargerIdInput: $chargerIdInput, transactionID: $transactionID))
+    }
+
+    func updateUIViewController(_ uiViewController: UIHostingController<KlarnaView>, context: UIViewControllerRepresentableContext<InitializeKlarna>) {
     }
 }
