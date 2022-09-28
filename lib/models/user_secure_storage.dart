d06c6a408b08694dea3ed5b383e3fea8d671ac5b
Magoulo <83534780+Magoulo@users.noreply.github.com>
2022-09-28 16:05:19 +0200

    successfully stores the accesstoken in secure storage

--- lib/models/user_secure_storage.dart
+++ lib/models/user_secure_storage.dart
@@ -2,13 +2,9 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class UserSecureStorage {
   // Create storage
-  static final _storage = new FlutterSecureStorage();
+  static final _storage = FlutterSecureStorage();
 
   static const _userAccessToken = "token";
-
-// Read value
-  static Future readUserAccessToken() async =>
-      await _storage.read(key: _userAccessToken);
 
 // Read all values
 //Map<String, String> allValues = await storage.readAll();
@@ -22,4 +18,9 @@ class UserSecureStorage {
 // Write value
   static Future setUserAccessToken(String AccessToken) async =>
       await _storage.write(key: _userAccessToken, value: AccessToken);
+
+// Read value
+  static Future<String?> getUserAccessToken() async {
+    await _storage.read(key: _userAccessToken);
+  }
 }
