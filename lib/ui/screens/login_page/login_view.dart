9f86676ddaf05bb821efcabbc5b16fc9ad428d91
Magoulo <83534780+Magoulo@users.noreply.github.com>
2022-09-29 10:45:18 +0200

    Solved the overflow issue in login view

--- lib/ui/screens/login_page/login_view.dart
+++ lib/ui/screens/login_page/login_view.dart
@@ -34,140 +34,121 @@ class _LoginViewState extends State<LoginView> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Flexible(
-                  flex: 2,
-                  child: Topbar(
-                      text: "Log In",
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => RegistrationView()),
-                        );
-                      }),
-                ),
-                // UPPP
-                // SizedBox(height: 30),
-                Flexible(
-                  flex: 4,
-                  child: Container(
-                    child: Column(
-                      // crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        ///Contains the TextInputs
-                        Flexible(
-                          flex: 2,
-                          child: Column(
-                            children: [
-                              FractionallySizedBox(
-                                child: UserFormInput(
-                                  controller: textControllerEmail,
-                                  isPassword: false,
-                                  hint: 'Enter Your Email',
-                                  labelText: 'Email',
-                                  validator: (email) {
-                                    if (email != null &&
-                                        !EmailValidator.validate(email) &&
-                                        email.isNotEmpty) {
-                                      return 'Enter a valid Email';
-                                    } else {
-                                      return null;
-                                    }
-                                  },
-                                ),
-                              ),
-                              SizedBox(height: 30),
-                              FractionallySizedBox(
-                                child: UserFormInput(
-                                  controller: textControllerPassword,
-                                  isPassword: true,
-                                  hint: 'Enter Your Password',
-                                  labelText: 'Password',
-                                  validator: (password) {
-                                    if (password != null &&
-                                        password.length < 3 &&
-                                        password.isNotEmpty) {
-                                      return 'Enter min. 3 characters';
-                                    } else {
-                                      return null;
-                                    }
-                                  },
-                                ),
-                              ),
-                            ],
+                Topbar(
+                    text: "Log In",
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => RegistrationView()),
+                      );
+                    }),
+                Container(
+                  child: Column(
+                    children: [
+                      Column(
+                        children: [
+                          FractionallySizedBox(
+                            child: UserFormInput(
+                              controller: textControllerEmail,
+                              isPassword: false,
+                              hint: 'Enter Your Email',
+                              labelText: 'Email',
+                              validator: (email) {
+                                if (email != null &&
+                                    !EmailValidator.validate(email) &&
+                                    email.isNotEmpty) {
+                                  return 'Enter a valid Email';
+                                } else {
+                                  return null;
+                                }
+                              },
+                            ),
                           ),
-                        ),
+                          SizedBox(height: 30),
+                          FractionallySizedBox(
+                            child: UserFormInput(
+                              controller: textControllerPassword,
+                              isPassword: true,
+                              hint: 'Enter Your Password',
+                              labelText: 'Password',
+                              validator: (password) {
+                                if (password != null &&
+                                    password.length < 3 &&
+                                    password.isNotEmpty) {
+                                  return 'Enter min. 3 characters';
+                                } else {
+                                  return null;
+                                }
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            errorMsg,
+                            style: TextStyle(color: Colors.red),
+                          ),
+                          Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 20)),
+                          WideButton(
+                            text: 'Log in',
+                            showWideButton: true,
+                            onTap: () async {
+                              print("Trying to log in");
 
-                        ///Contains the WideButton and The InkWell
-                        Flexible(
-                          flex: 1,
-                          child: Column(
-                            children: [
-                              Text(
-                                errorMsg,
-                                style: TextStyle(color: Colors.red),
-                              ),
-                              Padding(
-                                  padding: EdgeInsets.fromLTRB(0, 0, 0, 20)),
-                              WideButton(
-                                text: 'Log in',
-                                showWideButton: true,
-                                onTap: () async {
-                                  print("Trying to log in");
+                              var loginData = await model.validateLogin(
+                                  textControllerEmail.text,
+                                  textControllerPassword.text);
+                              print(
+                                  "loginData in view: " + loginData.toString());
+                              print("login contains isValid? : " +
+                                  loginData
+                                      .contains('errorMessage')
+                                      .toString());
+                              _validate = loginData.elementAt(0);
+                              if (!_validate) {
+                                setState(() {
+                                  errorMsg = loginData.elementAt(1);
+                                });
+                              } else {
+                                setState(() {
+                                  errorMsg = "";
+                                });
+                              }
+                              print(
+                                  "validation bool:  " + _validate.toString());
 
-                                  var loginData = await model.validateLogin(
-                                      textControllerEmail.text,
-                                      textControllerPassword.text);
-                                  print("loginData in view: " +
-                                      loginData.toString());
-                                  print("login contains isValid? : " +
-                                      loginData
-                                          .contains('errorMessage')
-                                          .toString());
-                                  _validate = loginData.elementAt(0);
-                                  if (!_validate) {
-                                    setState(() {
-                                      errorMsg = loginData.elementAt(1);
-                                    });
-                                  } else {
-                                    setState(() {
-                                      errorMsg = "";
-                                    });
-                                  }
-                                  print("validation bool:  " +
-                                      _validate.toString());
-
-                                  final isValidForm =
-                                      _formKey.currentState!.validate();
-                                },
+                              final isValidForm =
+                                  _formKey.currentState!.validate();
+                            },
+                            color: Color(0xff78bd76),
+                          ),
+                          SizedBox(height: 20),
+                          InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        RecoverPasswordView()),
+                              );
+                            },
+                            child: Text(
+                              'I forgot my password',
+                              style: TextStyle(
+                                fontFamily: 'Lato',
                                 color: Color(0xff78bd76),
+                                fontSize: 13,
+                                fontWeight: FontWeight.w700,
+                                fontStyle: FontStyle.normal,
                               ),
-                              SizedBox(height: 20),
-                              InkWell(
-                                onTap: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) =>
-                                            RecoverPasswordView()),
-                                  );
-                                },
-                                child: Text(
-                                  'I forgot my password',
-                                  style: TextStyle(
-                                    fontFamily: 'Lato',
-                                    color: Color(0xff78bd76),
-                                    fontSize: 13,
-                                    fontWeight: FontWeight.w700,
-                                    fontStyle: FontStyle.normal,
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
                   ),
                 ),
               ],
