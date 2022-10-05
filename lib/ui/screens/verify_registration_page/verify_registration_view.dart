68a676c3e2ec0ab7b88b9b1eb649e10d72d3d82f
Sabina <90258714+SeitansPilgrim@users.noreply.github.com>
2022-10-05 15:29:16 +0200

    Changed varialbel name

--- lib/ui/screens/verify_registration_page/verify_registration_view.dart
+++ lib/ui/screens/verify_registration_page/verify_registration_view.dart
@@ -82,7 +82,7 @@ class _VerifyRegistrationViewState extends State<VerifyRegistrationView> {
                                 onTap: () async {
                                   try {
                                     setState(() {
-                                      model.errors = "";
+                                      model.verificationErrors = "";
                                     });
 
                                     await model.verifyAccount();
@@ -93,14 +93,15 @@ class _VerifyRegistrationViewState extends State<VerifyRegistrationView> {
                                     );
                                   } catch (errors) {
                                     setState(() {
-                                      model.errors = errors.toString();
+                                      model.verificationErrors =
+                                          errors.toString();
                                     });
                                   }
                                 }),
 
                             if (!model.isAccountVerified &&
-                                model.errors.isNotEmpty)
-                              Text(model.errors,
+                                model.verificationErrors.isNotEmpty)
+                              Text(model.verificationErrors,
                                   style: TextStyle(color: Colors.red)),
 
                             // Text(model.errors.toString())
