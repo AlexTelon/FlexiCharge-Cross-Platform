de953d91698eb86c4b0325b6e990e5d3f2ffe9e3
Meles Abraha <meleskida@gmail.com>
2022-10-07 19:11:21 +0200

    bug-fix and refactoring view model

--- lib/ui/screens/recover_email_sent/recover_email_sent_viewmodel.dart
+++ lib/ui/screens/recover_email_sent/recover_email_sent_viewmodel.dart
@@ -1,28 +1,66 @@
+import 'package:flexicharge/models/user_input_validator.dart';
 import 'package:stacked/stacked.dart';
-
 import '../../../services/user_api_service.dart';
+
+const int VERIFICATION_CODE_LENGTH = 6;
 
 class RecoverEmailSentViewModel extends BaseViewModel {
   bool _checked = false;
+  String _password = "";
+  final _userInputValidator = UserInputValidator();
 
   set checked(newState) {
     _checked = newState;
     notifyListeners();
   }
 
+  bool get checked => _checked;
+
   Future<void> verifyPassword(String email, String password,
       String repeatPassword, String verificationCode) async {
-    if (password == repeatPassword) {
-      try {
-        await UserApiService()
-            .verifyPasswordReset(email, password, verificationCode);
-      } catch (error) {
-        print(error);
-      }
-    } else {
-      print("not the same passwords");
+    try {
+      await UserApiService()
+          .verifyPasswordReset(email, password, verificationCode);
+    } catch (error) {
+      print(error);
     }
   }
 
-  bool get checked => _checked;
+  String? validateVerificationCode(verificationCode) {
+    if (verificationCode != null &&
+        verificationCode.isNotEmpty &&
+        verificationCode.length < VERIFICATION_CODE_LENGTH) {
+      return 'Verification code needs to be ' +
+          VERIFICATION_CODE_LENGTH.toString() +
+          ' characters';
+    } else {
+      return null;
+    }
+  }
+
+  String? validatePassword(password) {
+    if (password != null && password.isNotEmpty) {
+      if (!_userInputValidator.passwordIsValid(password)) {
+        return _userInputValidator.passwordErrors.first;
+      } else {
+        _password = password;
+        return null;
+      }
+    } else {
+      return null;
+    }
+  }
+
+  String? validateRepeatedPassword(repeatedPassword) {
+    if (repeatedPassword != null &&
+        repeatedPassword.isNotEmpty &&
+        !_userInputValidator.passwordsAreEqual(_password, repeatedPassword)) {
+      if (_userInputValidator.passwordErrors.isNotEmpty) {
+        return 'Password must be valid';
+      } else
+        return 'Passwords do not match';
+    } else {
+      return null;
+    }
+  }
 }
