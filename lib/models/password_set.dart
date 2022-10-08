e4637e5ce8e864da9d520553010089e5a9d2c743
Gustav Persson <mailtogustav7@gmail.com>
2022-10-08 14:51:42 +0200

    Add unit test for client-side password match check

---
+++ lib/models/password_set.dart
@@ -0,0 +1,6 @@
+class PasswordPair {
+  String password1;
+  String password2;
+
+  PasswordPair(this.password1, this.password2);
+}
--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,13 +1,14 @@
-// This file contains a Flutter unit test.
+// This file contains Flutter unit tests.
 
+import 'package:flexicharge/models/password_set.dart';
 import 'package:flexicharge/models/user_input_validator.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('Test client-side password validataion', () {
-    //ARRANGE
-    UserInputValidator userInputValidator = new UserInputValidator();
+  //ARRANGE
+  UserInputValidator userInputValidator = new UserInputValidator();
 
+  group('Test client-side password validataion', () {
     const inputsToExpected = <String, bool>{
       'pegu20ju@@': false,
       'Test.1234': true,
@@ -15,13 +16,13 @@ void main() {
       'Test.12': false,
       '': false,
       'abcdefghijklmno': false,
-      '_123Test.se': true
+      '_123Test.se': true,
     };
 
     inputsToExpected.forEach((input, expected) {
       test('$input -> $expected', () {
         //ACT
-        bool result = userInputValidator.passwordIsValid(input);
+        final bool result = userInputValidator.passwordIsValid(input);
 
         //ASSERT
         expect(result, expected);
@@ -30,9 +31,6 @@ void main() {
   });
 
   group('Test client-side email validation', () {
-    //ARRANGE
-    UserInputValidator userInputValidator = new UserInputValidator();
-
     const inputsToExpected = <String, bool>{
       'pegu20ji@student.ju.se': true,
       'liti20dt@student.ju.se': true,
@@ -40,13 +38,32 @@ void main() {
       'pegu20ji': false,
       'pegu20ji@': false,
       'test@test.se': true,
-      'test.testsson@edu.jonkoping.se': true
+      'test.testsson@edu.jonkoping.se': true,
     };
 
     inputsToExpected.forEach((input, expected) {
       test('$input -> $expected', () {
         //ACT
-        bool result = userInputValidator.emailIsValid(input);
+        final bool result = userInputValidator.emailIsValid(input);
+
+        //ASSERT
+        expect(result, expected);
+      });
+    });
+  });
+
+  group('Test client-side password match', () {
+    final inputsToExpected = <PasswordPair, bool>{
+      PasswordPair('Test.1234', 'Test.1234'): true,
+      PasswordPair('test.1234', 'Test.1234'): false,
+      PasswordPair('teståäö', 'teståäö'): true,
+      PasswordPair('postgres.', 'postgres'): false,
+    };
+    inputsToExpected.forEach((input, expected) {
+      test('${input.password1} == ${input.password2} -> $expected', () {
+        //ACT
+        final bool result = userInputValidator.passwordsAreEqual(
+            input.password1, input.password2);
 
         //ASSERT
         expect(result, expected);
