19238d5cc8759cc32796ce4b706a7f0b9955b01d
Afram7 <haaf20ml@student.ju.se>
2023-09-17 18:04:19 +0200

    Improved the spacing.
    Improved the spacing between "Change password" and "Update" buttons. It's now based on the device's hight to prevent scrolling.

--- lib/ui/screens/account_settings_page/account_settings_view.dart
+++ lib/ui/screens/account_settings_page/account_settings_view.dart
@@ -7,95 +7,106 @@ import '../../widgets/text_input.dart';
 import '../../widgets/top_bar.dart';
 import 'account_settings_viewmodel.dart';
 
-
 class AccountSettingsView extends StatelessWidget {
-final TextEditingController emailController = TextEditingController();
-final TextEditingController mobileNumberController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController mobileNumberController = TextEditingController();
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double desiredPercentage = 25;
 
-  void _showAlertDialog(BuildContext context) {
-    showCupertinoModalPopup<void>(
-      context: context,
-      builder: (BuildContext context) => CupertinoAlertDialog(
-        title: const Text('Delete Account'),
-        content: const Text('Are you sure you want to delete your Flexicharge user account?'),
-        actions: <CupertinoDialogAction>[
-          CupertinoDialogAction(
-            /// This parameter indicates this action is the default,
-            /// and turns the action's text to bold text.
-            isDefaultAction: true,
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: const Text('Cancel'),
-          ),
-          CupertinoDialogAction(
-            /// This parameter indicates the action would perform
-            /// a destructive action such as deletion, and turns
-            /// the action's text color to red.
-            isDestructiveAction: true,
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: const Text('Delete'),
-          ),
-        ],
-      ),
-    );
-  }
+    void _showAlertDialog(BuildContext context) {
+      showCupertinoModalPopup<void>(
+        context: context,
+        builder: (BuildContext context) => CupertinoAlertDialog(
+          title: const Text('Delete Account'),
+          content: const Text(
+              'Are you sure you want to delete your Flexicharge user account?'),
+          actions: <CupertinoDialogAction>[
+            CupertinoDialogAction(
+              /// This parameter indicates this action is the default,
+              /// and turns the action's text to bold text.
+              isDefaultAction: true,
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Cancel'),
+            ),
+            CupertinoDialogAction(
+              /// This parameter indicates the action would perform
+              /// a destructive action such as deletion, and turns
+              /// the action's text color to red.
+              isDestructiveAction: true,
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Delete'),
+            ),
+          ],
+        ),
+      );
+    }
 
-    return ViewModelBuilder<AccountSettingsViewModel>.reactive(viewModelBuilder: () => AccountSettingsViewModel(),
+    return ViewModelBuilder<AccountSettingsViewModel>.reactive(
+      viewModelBuilder: () => AccountSettingsViewModel(),
       builder: (context, model, child) => Scaffold(
         body: SingleChildScrollView(
-          child: 
-          Column(
+          child: Column(
             children: <Widget>[
-            Container(
-              child: Topbar(
-                text: "Account \nSettings",
-                onTap: () { Navigator.pop(context);},
+              Container(
+                child: Topbar(
+                  text: "Account \nSettings",
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
               ),
-            ),
-
-            Container(
-              padding: EdgeInsets.fromLTRB(0, 40, 0, 30),
-              child: TextInputWidget(controller: emailController, labelText: "Email", hint: "Your email", onChanged: (value) => model.updateEmail(value)),
-            ),
-
-            Container(
-            padding: EdgeInsets.fromLTRB(0, 0, 0, 25),
-            child: TextInputWidget(controller: mobileNumberController, labelText: "Mobile Number", hint: "Your mobile number", onChanged: (value) => model.updateMobileNumber(value)),
-            ),
-
-            Container(
-              padding: const EdgeInsets.fromLTRB(0, 0, 0, 280),
-              child: TextButton(
-                style: TextButton.styleFrom(
-                  textStyle: const TextStyle(fontSize: 15),
-                  foregroundColor: FlexiChargeTheme.green),
-              onPressed: () {},
-              child: const Text('Change Password'),
-            )),
-
-            Container(
-              padding: EdgeInsets.fromLTRB(20, 0, 20, 10),
-              child: WideButton(
-                color: FlexiChargeTheme.green,
-                text: "Update",
-                onTap: () {},
-                showWideButton: true,
+              Container(
+                padding: EdgeInsets.fromLTRB(0, 40, 0, 30),
+                child: TextInputWidget(
+                    controller: emailController,
+                    labelText: "Email",
+                    hint: "Your email",
+                    onChanged: (value) => model.updateEmail(value)),
               ),
-            ),
-
-            Container(
-              child: TextButton(
+              Container(
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 25),
+                child: TextInputWidget(
+                    controller: mobileNumberController,
+                    labelText: "Mobile Number",
+                    hint: "Your mobile number",
+                    onChanged: (value) => model.updateMobileNumber(value)),
+              ),
+              Container(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                        textStyle: const TextStyle(fontSize: 15),
+                        foregroundColor: FlexiChargeTheme.green),
+                    onPressed: () {},
+                    child: const Text('Change Password'),
+                  )),
+              Container(
+                padding: EdgeInsets.only(
+                    bottom: screenHeight * (desiredPercentage / 100)),
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(20, 0, 20, 10),
+                child: WideButton(
+                  color: FlexiChargeTheme.green,
+                  text: "Update",
+                  onTap: () {},
+                  showWideButton: true,
+                ),
+              ),
+              Container(
+                  child: TextButton(
                 style: TextButton.styleFrom(
-                  textStyle: const TextStyle(fontSize: 15),
-                  foregroundColor: FlexiChargeTheme.red),
-              onPressed: () => _showAlertDialog(context),
-              child: const Text('Delete my account'),
-            )),
+                    textStyle: const TextStyle(fontSize: 15),
+                    foregroundColor: FlexiChargeTheme.red),
+                onPressed: () => _showAlertDialog(context),
+                child: const Text('Delete my account'),
+              )),
             ],
           ),
         ),
